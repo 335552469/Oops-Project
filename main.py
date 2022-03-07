e73f44cb46527ad645ed79b83168faedcc9becd9
335552469 <93568626+335552469@users.noreply.github.com>
2022-03-07 10:51:25 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame as pg
+pg.init()
+
+surface = pg.display.set_mode((500, 500))
+run = True
+
+class Box(object):
+    def __init__(self):
+        pass
+
+    def draw_box(self):
+        pass
+
+    def glow(self):
+        pass
+    
+
+while run: 
+    for event in pg.event.get():
+        if event.type == pg.QUIT: 
+            run = False
+
+    surface.fill((23, 85, 255))
+
+
+    pg.display.update()
+            
