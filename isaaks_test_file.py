eea873901f76a5e056b353b68d5402d29c4984b2
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2022-03-19 17:24:33 -0400

    asd

--- isaaks_test_file.py
+++ isaaks_test_file.py
@@ -93,7 +93,7 @@ def new_sequence():
     sound.shuffle()
 
     for i in sequence:
-        box_matrix[i[0]][i[1]].glow(0.5,(255,255,0))
+        box_matrix[i[0]][i[1]].glow(1,(0,0,255))
 
 
 highscore = HighScore()
