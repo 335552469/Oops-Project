dffa9ef844201806b9bdecd8c626e826128eefa8
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2022-03-19 15:56:58 -0400

    done idk

--- amir_testing_file.py
+++ amir_testing_file.py
@@ -21,6 +21,7 @@ class Box(object):
         self.height = height
         self.order = order
         self.box_colour = (37,115,193)
+        self.default_color = self.box_colour
 
     def draw_box(self, surface): # draws box
         pygame.draw.rect(surface, self.box_colour, (self.x, self.y, self.width, self.height), 0, 9)
@@ -31,28 +32,45 @@ class Box(object):
                 return True
         else:
             return False
-    def glow(self):
-        pass
+
+    def interpolate_color(self, start_time, max_fade_time, color):
+        fract = (time.time() - start_time) / (max_fade_time)
+        if fract > 0.5:
+            fract = 1 - fract * 1.3
+        color1 = pygame.Color(*self.default_color)
+        if fract < 0:
+            return pygame.Color(*self.default_color)
+        return color1.lerp(color, fract)
+
+    def glow(self,length):
+        
+        start_time = time.time()
+        while True:
+            time.sleep(0.01)
+            current_color = self.interpolate_color(start_time,length,pygame.Color(255,255,255))
+            self.box_colour = current_color
+            self.draw_box(surface)
+            pygame.display.flip()
+            if start_time+length < time.time():
+                break
 
 
 class Sequence(object):
     def __init__(self, sequence, seq_amount):
         self.sequence = sequence
         if (seq_amount - len(self.sequence)) > 0:
-            self.sequence.append([[random.randint(1,2),random.randint(1,2)] for i in range(seq_amount - len(self.sequence))])
-        print(self.sequence)
+            for i in range(seq_amount - len(self.sequence)):
+                self.sequence.append([random.randint(0,2),random.randint(0,2)])
 
     def check_sequence(self, sequence):
+        print(self.sequence,"should equal",sequence)
         if self.sequence == sequence:
             return True
         return False
     
     def reveal_sequence(self, cards, level):
-        for i in cards:
-            for j in i:
-                print(j)
-                j.glow()
-                time.sleep(2/(level)*1.2)
+        for i in self.sequence:
+            cards[i[0]][i[1]].glow(1)
 
 
 class Sound:
@@ -67,17 +85,17 @@ class Sound:
         self.shuffle_sound.play()
 
 
-box_matrix = [[Box((j)*137+53, (i)*137+53, 120, 120, [j,i]) for j in range(3)] for i in range(3)]
+box_matrix = [[Box((j)*137+53, (i)*137+53, 120, 120, [i,j]) for j in range(3)] for i in range(3)]
 sequence_obtained = []
-level_sequence = Sequence([[random.randint(1,2),random.randint(1,2)] for i in range(level)], level)
+level_sequence = Sequence([[random.randint(1,2),random.randint(1,2)] for i in range(level)], 1)
 
 
 print("Level",level,"| Winning Sequence: ",level_sequence.sequence)
 
+soundboard = Sound()
 
 while True:
     surface.fill((43 ,135 ,209))
-
     for i in box_matrix:
         for j in i:
             j.draw_box(surface)
@@ -91,15 +109,20 @@ while True:
             for i in box_matrix:
                 for j in i:
                     if j.mouse_detection(pygame.mouse.get_pos()) == True:
+                        soundboard.cardflip()
+                        j.glow(1)
                         sequence_obtained.append(j.order)
-                        print(sequence_obtained)
-                        print(level, len(sequence_obtained))
         if len(sequence_obtained) == level:
             if level_sequence.check_sequence(sequence_obtained):
                 print("you win")
                 level += 1
             else:
                 print("you lose")
+                level = 1
+                level_sequence = Sequence(level_sequence.sequence, level)
+                print(level_sequence.sequence)
+                level_sequence.reveal_sequence(box_matrix, level)
+                
             sequence_obtained = []
             level_sequence = Sequence(level_sequence.sequence, level)
             level_sequence.reveal_sequence(box_matrix, level)
