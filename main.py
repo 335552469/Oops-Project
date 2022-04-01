57e9c471954c5c299c9fe1ae5a46aa3788d243c4
jonnykoven <77310447+jonnykoven@users.noreply.github.com>
2022-04-01 12:05:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 from msilib import sequence                 #
-from multiprocessing.connection import wait # Imports
+from multiprocessing.connection import wait
+from re import T # Imports
 import pygame, sys, random, time, math      #
-import threading
 pygame.init()
 
 screenX, screenY = 500, 500 # Dimensions of the game window
 surface = pygame.display.set_mode((screenX, screenY))
+pygame.display.set_caption("Sequence Game") # Sets the caption
 
 run = True
 play = 0 # 0 = Open screen, 1 = main game, 2 = death screen
 correctClicks = 0
 lose = False
-
-def thread(function):
-    def fix(*x, **y):
-        threading.Thread(target=function, args=x, kwargs=y).start()
-    return fix
-
 class Box(object):
-
-    def __init__(self, x, y, width, height, order): # initializes values of the boxes 
+    def __init__(self, x, y, width, height, order): # innitializes values of the boxes 
         self.x = x
         self.y = y
         self.width = width
@@ -27,10 +21,12 @@ class Box(object):
         self.order = order
         self.boxColour = [37,115,193]
         self.defaultColor = self.boxColour
-
+        self.endcolor = self.boxColour
+        self.cycle = False
+        self.donecycle = False
+        
     def drawBox(self, surface): # draws a box
         pygame.draw.rect(surface, self.boxColour, (self.x, self.y, self.width, self.height), 0, 9)
-
     def mouseDetection(self, mouse):
         if mouse[0] >= self.x and mouse[0] <= self.x + self.width:
             if mouse[1] >= self.y and mouse[1] <= self.y + self.height: # Checks if the mouse is inside any given box and returns a boolean
@@ -38,88 +34,75 @@ class Box(object):
         else:
             return False
 
-    def interpolateColor(self, startTime, maxFadeTime, color): # Interpolates the fading of a given color based on the starting color,
-                                                               # end color, and time.
-        fract = (time.time() - startTime) / (maxFadeTime)
-        if fract > 0.5:
-            fract = 1 - fract * 1.3
-        color1 = pygame.Color(*self.defaultColor)
-        if fract < 0:
-            return pygame.Color(*self.defaultColor)
-        return color1.lerp(color, fract)
+    def glow(self, endcolor= [37,115,193]):
+        self.endcolor = endcolor
+        #print(self.boxColour, end=" ")
+        #print(self.endcolor)
+        
+        rate = 1
 
-    @thread
-    def glow(self,length,color,waitTime):
-        time.sleep(waitTime)
-        startTime = time.time() # Fades the color of a box in and out
-        for i in range(math.floor(length*100)):
-            currentColor = self.interpolateColor(startTime,length,pygame.Color(color))
-            self.boxColour = currentColor
-            #self.drawBox(surface)
-            #pygame.display.flip()
-            time.sleep(0.01)
-        return 1354
+        for i in range(3):
+            if not self.cycle:                  # first cycle
+                if self.boxColour[i] >= self.endcolor[i] + rate:
+                    self.boxColour[i] -= rate
+                
+                if self.boxColour[i] <= self.endcolor[i] - rate:
+                    self.boxColour[i] += rate
+                
+                if self.boxColour == self.endcolor:
+                    self.cycle = True
+            else:                               # second cycle
+                if self.boxColour[i] >= self.defaultColor[i] + rate:
+                    self.boxColour[i] -= rate
+                
+                if self.boxColour[i] <= self.defaultColor[i] - rate:
+                    self.boxColour[i] += rate
+                
+                if self.boxColour == self.defaultColor:
+                    self.cycle = False
+                    self.endcolor = [37,115,193]
+                    self.donecycle = True
 
-    def freezeGlow(self,length,color,waitTime):
-        time.sleep(waitTime)
-        startTime = time.time() # Fades the color of a box in and out without threading
-        for i in range(math.floor(length*100)):
-            currentColor = self.interpolateColor(startTime,length,pygame.Color(color))
-            self.boxColour = currentColor
-            self.drawBox(surface)
-            pygame.display.flip()
-            time.sleep(0.01)
+
 
 class HighScore:
     def __init__(self): # Initiates values for highscore
         self.score = 0
-
     def readScore(self):
         try:
             with open("highscore.txt", "r") as file: 
                 self.score = str(file.readline()) # reads the "highscore.txt" file to see what the players highscore is
         except FileNotFoundError:
             print("No high score yet.") # Checks if the "highscore.txt" file exists
-
     def writeScore(self, score):
         with open("highscore.txt", "w") as file:
             file.write(str(score)) # updates the "highscore.txt" file with the players highscore
-
     def __str__(self):
         return f"The high score is {self.score}" # Text to present the highscore on the screen
-
-
-
 class Sound:
     def __init__(self): # Initiates sounds
         self.cardflipSound = pygame.mixer.Sound("deep.mp3")
         self.shuffleSound = pygame.mixer.Sound("vine.mp3")
-
     def cardflip(self):
         self.cardflipSound.play() # Plays the sound the happens when a box is clicked
-
-    @thread
     def shuffle(self):
-        time.sleep(0.5)
         self.shuffleSound.play() # Plays the sound that happens at the beginning of each round
-
 sound = Sound()
-
 boxMatrix = [[Box((j)*137+53, (i)*137+53, 120, 120, j) for j in range(3)] for i in range(3)] # Creates the matrix for the 9 boxes
 sequence = []
-
 def newSequence(): # Controls the sequence of flashing boxes
     x = random.randint(0, 2)
     y = random.randint(0, 2)
     sequence.append([x, y]) # adds a specific box into the sequence
     sound.shuffle() # plays the next round sound
-    for i in range(len(sequence)):
-        x = boxMatrix[sequence[i][0]][sequence[i][1]].glow(1 * (1 - 0.05 * len(sequence)),(255,255,255),i+1)
-
-
-
+    for i in sequence:
+        enumerate = sequence.index(i)
+        print(sequence)
+        if enumerate == 0:
+                boxMatrix[i[0]][i[1]].glow([255,255,255]) # makes the random box in the sequence glow 
+        elif boxMatrix[sequence[enumerate-1][0]][sequence[enumerate-1][1]].donecycle == True :
+                boxMatrix[i[0]][i[1]].glow([255,255,255]) # makes the random box in the sequence glow 
 highScore = HighScore()
-
 highScore.readScore()
 score = 0
 class Screen:
@@ -129,7 +112,6 @@ class Screen:
         self.lose = lose
         self.correctClicks = correctClicks
         self.score = score
-
     def start(self):
         while self.play == 0: # While loop for the opening screen
             for event in pygame.event.get():
@@ -143,15 +125,12 @@ class Screen:
             font = pygame.font.SysFont('Cooper', 50)
             write = font.render("Sequence Memory Test", True, (255, 255, 255))
             surface.blit(write, (55, 230)) # Writes "Sequence Memory Test" on the screen
-
             font = pygame.font.SysFont('Cooper', 30)
             write = font.render("Memorize the pattern", True, (255, 255, 255))
             surface.blit(write, (140, 265)) # Writes "Memorize the pattern" on the screen
-
             font = pygame.font.SysFont('Cooper', 40)
             write = font.render("Press any key to begin", True, (255, 255, 255))
             surface.blit(write, (105, 310))  # Writes "Press any key to begin" on the screen
-
             pygame.draw.rect(surface, (255, 255, 255), (190, 75 , 50 , 50), 0, 9)  #
             pygame.draw.rect(surface, (255, 255, 255), (250, 75 , 50 , 50), 0, 9)  #
                                                                                 # 
@@ -159,20 +138,16 @@ class Screen:
             pygame.draw.rect(surface, (255, 255, 255), (250, 135 , 50 , 50), 0, 9) #
                                                                                 #
             pygame.draw.rect(surface, (43, 135, 209), (257, 143, 35 , 35), 0, 9)   #
-
             pygame.display.update() # Updates the screen
-
     def main(self):
         while self.play == 1: # While loop for the main game
             surface.fill((43 ,135 ,209)) # fills the screen a certain colour
             font = pygame.font.SysFont('Cooper', 30)
             write = font.render("HighScore: " + str(highScore.score) + "                               Level: " + str(self.score+1), True, (0, 0, 0))
             surface.blit(write, (55, 20)) # Writes the highscore and the level on the screen
-
             for i in boxMatrix:
                 for j in i:
                     j.drawBox(surface) # draws the 9 boxes according to the box matrix
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -186,13 +161,13 @@ class Screen:
                                     self.correctClicks += 1
                                     if self.correctClicks == len(self.sequence)-1:
                                         self.score +=1 # adds to the plaers score
-                                    j.glow(0.5,(0,255,0),0) # makes the box glow white if the player has clicked the correct box
+                                    j.glow([0,255,0]) # makes the box glow white if the player has clicked the correct box
                                 else: # if the player correctClicks the wrong box
                                     if self.score >= int(highScore.score): # Checks if the player has beat their highscore
                                         highScore.score = str(self.score+1)
                                         highScore.writeScore(str(self.score+1)) # Adds the new highscore to the "highscore.txt" file
                                     
-                                    j.freezeGlow(0.5,(255,0,0),0) # Makes the box glow red if the player has clicked the wrong box
+                                    j.glow([255,0,0]) # Makes the box glow red if the player has clicked the wrong box
                                     self.correctClicks = 0         #
                                     self.sequence = []              #
                                     self.score = 0                  # Resets correctClicks, the box sequence, the score, stops this while loop, and starts the lose screen while loop
@@ -200,18 +175,18 @@ class Screen:
                                     self.play = 2                   #
                                     break
                                     
-
-
-            if self.correctClicks == len(self.sequence) and self.play != 2: # checks if the player has completed the round
+            if self.correctClicks == len(self.sequence): # checks if the player has completed the round
                 newSequence() # Starts the next round
                 self.correctClicks = 0
             
             if self.lose: # Checks if the player has lost
                 newSequence() # Starts the new round
                 self.lose = False
-                
+            
+            for i in range(3):
+                for j in range(3):
+                    boxMatrix[i][j].glow(boxMatrix[i][j].endcolor)
             pygame.display.update() # Updates the screen
-
     def death(self):
         while self.play == 2: # While loop for the lose screen
             for event in pygame.event.get():
@@ -221,14 +196,10 @@ class Screen:
                     sys.exit() # closes the game
                 if event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                     self.play = 1 # Stops this loop and starts the one for the main game
-
-
             surface.fill((43 ,135 ,209)) # Fills the creen a certain colour
-
             font = pygame.font.SysFont('Cooper', 50)
             write = font.render("Sequence Memory Test", True, (255, 255, 255))
             surface.blit(write, (55, 230)) # Writes "Sequence Memory Test" on the screen
-
             font = pygame.font.SysFont('Cooper', 30)
             write = font.render("Press any key to try again.", True, (255, 255, 255))
             surface.blit(write, (120, 300)) # Writes "Press any key to try again." on the screen
@@ -240,10 +211,8 @@ class Screen:
             pygame.draw.rect(surface, (255, 255, 255), (250, 135 , 50 , 50), 0, 9) #
                                                                                 #
             pygame.draw.rect(surface, (43, 135, 209), (257, 143, 35 , 35), 0, 9)   #
-
             pygame.display.update() # Updates the screen
             self.lose = True
-
 mainGame = Screen(play, sequence, lose, correctClicks, score)
 while True:
     mainGame.start()
